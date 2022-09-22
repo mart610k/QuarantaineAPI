263417e6636165b515523a0959acc96fa0212f86
Martin <darkmtbg2@gmail.com>
2022-09-22 14:25:50 +0200

    Refactor

--- src/main/java/dk/quarantaine/API/application/helper/FomatHelper.java
+++ src/main/java/dk/quarantaine/API/application/helper/FomatHelper.java
@@ -1,14 +1,11 @@
 package dk.quarantaine.api.application.helper;
 
 public class FomatHelper {
-
-
-
+    
     public static boolean validatePassword(String toValidate){
         return (toValidate == null ? false : toValidate.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%#^*?&])[A-Za-z\\d@$!#%^*?&]{8,}$"));
         
     }
-
 
     public static boolean validatePhoneNumber(String toValidate) {
         return (toValidate == null ? false : toValidate.matches("^[0-9]{8}$"));
